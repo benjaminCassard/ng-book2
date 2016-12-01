c75e44c398975f9d6e8504d7f19bdc3afdac03c4
benjaminCassard <benjamin.cassard@gmail.com>
2016-12-01 17:59:28 +0100

    BCA - Change app-root to inventory-app.

--- inventory_app/e2e/app.po.ts
+++ inventory_app/e2e/app.po.ts
@@ -6,6 +6,6 @@ export class InventoryAppPage {
   }
 
   getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+    return element(by.css('inventory-app h1')).getText();
   }
 }
--- inventory_app/src/app/app.component.ts
+++ inventory_app/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'inventory-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+}
+
+export class Product {
+  constructor(
+    public sku: string,
+    public name: string,
+    public imageUrl: string,
+    public department: string[],
+    public price: number) {
+  }
 }
