2cf78a6586cbb5f20cad0f7166c5143a4903ac61
benjaminCassard <benjamin.cassard@gmail.com>
2016-12-01 07:27:00 +0100

    BCA - Add inventory app.

---
+++ inventoryApp/src/app.ts
@@ -0,0 +1,8 @@
+/**
+ * Provides a Product object
+ */
+class Product {
+  constructor(public sku: string, public name: string, public imageUrl: string, public department: string[], public price: number){
+
+  }
+}
