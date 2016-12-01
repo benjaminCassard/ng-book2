7fadb750054e256dacd1f1a1e38f7afcb1c48128
benjaminCassard <benjamin.cassard@gmail.com>
2016-12-01 08:35:43 +0100

    BCA - Remove old project.

--- inventoryApp/src/app.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Provides a Product object
- */
-class Product {
-  constructor(public sku: string, public name: string, public imageUrl: string, public department: string[], public price: number){
-
-  }
-}
